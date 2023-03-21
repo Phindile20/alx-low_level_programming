90a95d653453385a1747cc82fcfff9e22f44ddc1
Your Name <you@example.com>
2023-03-21 14:10:46 -0700

    0x02-functions nested loop task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints all the alphabet in lowercase
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: On success 1.
  */
-
 void print_alphabet(void)
 {
-	char letter;
+	int letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		_putchar(letter);
-	
+	}
 	_putchar('\n');
 }
