ed6e8ee8921bd5d45a951fd1d4ba7702df55e276
Your Name <you@example.com>
2023-04-05 13:03:02 -0700

    0x08 recursion task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Write a function that prints a string
+ * @s : char
+ * Return : void
+ */
+
+void _puts_recursion(char *s);
+{
+	puts(s);
+}
