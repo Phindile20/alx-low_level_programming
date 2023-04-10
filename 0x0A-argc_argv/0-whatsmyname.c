2b2e7bcbe8f87c7c78bb1df3a9bc97c55406e925
Your Name <you@example.com>
2023-04-10 11:36:23 -0700

    0x0A argc argv task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - to print the name of the program
+ *
+ * @argc: unused variable
+ *
+ * @argv: array strings
+ *
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
