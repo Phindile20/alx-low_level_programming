1f298518bdb586912462c9e26165e6d412771b54
Your Name <you@example.com>
2023-04-14 09:08:26 -0700

    0x0C malloc free task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,4 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _calloc -  function that allocates memory for an array, using malloc
  *
+ * nmemb: memory for an array
+ *
+ * size: bytes size of array
+ *
+ * Return: poiner to the address of the memory block
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ray;
+	unsigned int i;
+
+	while (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ray = malloc(nmemb * size);
+
+	if (ray != NULL)
+	{
+		for (i = 0; i < (nmemb * size); i++)
+			ray[i] = 0;
+		return (ray);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
