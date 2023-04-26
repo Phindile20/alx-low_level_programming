8906f1880d368e24bcf4ba34e2ed9c0868048d3d
Your Name <you@example.com>
2023-04-26 12:10:48 -0700

    0x0 recursion task 4

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,6 +14,6 @@ void _puts_recursion(char *s)
 		putchar('\n');
 		return;
 	}
-	_puts_recursion((s + 1));
 	putchar(*s);
+	_puts_recursion((s + 1));
 }
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,1 +1,23 @@
-
+#include "main.h"
+/**
+ * _pow_recursion - a function that returns the value
+ * of x raised to the power of y.
+ *
+ * @x: number being manipulated
+ *
+ * @y: number that manipulates
+ *
+ * Return: sum of int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (!y)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, --y));
+}
