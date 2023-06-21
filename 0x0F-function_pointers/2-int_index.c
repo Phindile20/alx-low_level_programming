e7c01eaa6ce0d1936d9355b8588d74ebb4f22fcd
Phindile20 <phindileayandasibiya6@gmail.com>
2023-06-21 16:22:11 -0700

    function pointer  tasks

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,15 +17,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	while (!array || !cmp)
-	{
-		if (size <= 0)
-			return (-1);
-
-		for (i = 0; i < size; i++)
-			while (cmp(array[1]))
+	if (size > 0 && cmp && size && array)
+		for (i = 0; i <= size; i++)
+			if (cmp(array[i]))
 				return (i);
-	}
-
 	return (-1);
 }
